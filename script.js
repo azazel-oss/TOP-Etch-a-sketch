fc4c38fcf1cad4f7fef7cf52249858159a957644
Asad Mahmood <asadmhmd1234@gmail.com>
2022-05-03 16:55:39 +0530

    Add the starter files and complete basic etch-a-sketch features

---
+++ script.js
@@ -0,0 +1,25 @@
+const paletteEl = document.querySelector(".palette");
+const gridPromptBtn = document.querySelector(".grid-prompt");
+
+gridPromptBtn.addEventListener("click", () => {
+  let size = Number.parseInt(prompt("Enter the grid size(4-64):"));
+
+  let child = paletteEl.lastElementChild;
+  while (child) {
+    paletteEl.removeChild(child);
+    child = paletteEl.lastElementChild;
+  }
+
+  for (let i = 0; i < size * size; i++) {
+    const divEl = document.createElement("div");
+    divEl.classList.add("box");
+    divEl.style.width = 560 / size - 2 + "px";
+    paletteEl.appendChild(divEl);
+  }
+  const boxListEl = document.querySelectorAll(".box");
+  boxListEl.forEach((box) => {
+    box.addEventListener("mouseenter", (event) => {
+      event.target.style.backgroundColor = "red";
+    });
+  });
+});
